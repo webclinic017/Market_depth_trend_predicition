fef38c4030317060696dad10fd14768550e0da8b
spawnaga <53195435+spawnaga@users.noreply.github.com>
2022-02-23 00:00:24 -0700

    Update Market_depth_trend_prediction_model.py
    switched Dense layers with LSTM and removing shuffling data

--- Market_depth_trend_prediction_model.py
+++ Market_depth_trend_prediction_model.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 28 20:38:29 2020
+Created on Tus Feb 22 2:38:29 2022
 
 @author: alial
+"""
+
+
+""" updated from previous version. I followed suggestions from reddit members 
+morphicon : suggested to remove shuffle in data distribution and use RNN, LSTM Or XGBoost
+and Very_Large_Cone which suggested to not shuffle the data and split on a time-series
 """
 
 # imports
@@ -14,7 +20,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.layers import Input, Dense, Dropout, LSTM, GlobalAveragePooling1D
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.optimizers import Adam
 
@@ -37,7 +43,7 @@ config.gpu_options.allow_growth = True
 # End of GPU setup
 
 
-db = sqlite3.connect(r'C:\Udemy\Interactive Brokers Python API\streaming ES\ES_ticks1.db') # Loading the file
+db = sqlite3.connect(r'C:\Udemy\Interactive Brokers Python API\streaming ES\ES_ticks.db') # Loading the file
 df = pd.read_sql_query("SELECT * from ES_market_depth", db) # converting file into a Dataframe
 df['difference'] = df['lastPrice'].shift(1)-df['lastPrice'] # setting the difference between two tics (current and last tick) and shifting one row up 
 df.dropna(inplace = True) 
@@ -66,7 +72,7 @@ df = pd.concat(objs = (pd.DataFrame(df[df['same']>0].iloc[0:len(df[df['down_tren
 X = df.iloc[:,1:-6].values
 y = df.iloc[:,-3:].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle= False) #Shuffle set to False
 
 
 #Normalizing data
@@ -78,23 +84,21 @@ X_test = scaler.transform(X_test)
 
 
 #setting up the model of tensorflow
-input_layer = Input(shape=(X.shape[1]))
-x = Dense(100, activation='relu')(input_layer)
-x = Dropout(0.5)(x)
-x = Dense(50, activation='relu')(x)
-x = Dropout(0.5)(x)
-x = Dense(25, activation='relu')(x)
-x = Dropout(0.5)(x)
+input_layer = Input(shape=(X.shape[1],1))
+x=input_layer
+for _ in range(5): # five layers
+       x = Dropout(0.2)(x) # Dropout to avoid overfitting
+       x = LSTM(50, return_sequences = True)(x) # using LSTM insted of Dense with return sequences to adopt the time sequences
+x = GlobalAveragePooling1D()(x) # Global averaging to one layer shape to feed to a dense categorigal classification
 output = Dense(y.shape[1], activation='softmax')(x)
 model = Model(inputs=input_layer, outputs=output)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics = ['acc'])
 
 #creating an early stop based on minmizing val_loss
-early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
+early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=200)
 
 #fit the model
 r = model.fit(X_train, y_train, epochs = 200000, batch_size=512,
-             validation_data = (X_test, y_test), callbacks=[early_stop], shuffle=0.5)
+             validation_data = (X_test, y_test), callbacks=[early_stop], shuffle=False) #fit the data without shuffling
 #plot the results.
 pd.DataFrame(r.history).plot()
-
